afa59a1384946c44b00f7bec724d627faa350747
Naveen Manjappa <NManjappa@manh.com>
2024-02-05 19:57:13 +0000

    Dynamic components using component factory resolver

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit, ViewChild, inject } from '@angular/core';
 import { User } from '../Models/User';
 import { UserService } from '../services/user.service';
+import { ConfirmDeleteComponent } from './confirm-delete/confirm-delete.component';
+import { ViewContainer } from '../Directives/viewContainer.directive';
 
 @Component({
   selector: 'app-users',
@@ -12,14 +14,21 @@ export class UsersComponent implements OnInit {
   showConfirmDeleteComp:boolean=false;
   userService=inject(UserService);
   userToDelete:User;
+  @ViewChild(ViewContainer,{static:false}) container:ViewContainer;
 
+  OnConfirmationObs;
+
+  constructor(private componentFactoryRes:ComponentFactoryResolver){
+
+  }
   ngOnInit(): void {
     this.users=this.userService.users;
   }
 
   OnDeleteClicked(user:User){
-    this.showConfirmDeleteComp=true;
-    this.userToDelete=user;
+    // this.showConfirmDeleteComp=true;
+     this.userToDelete=user;
+    this.showConfirmDelete(this.userToDelete);
   }
   OnUserConfirmation(value:boolean){
     this.showConfirmDeleteComp=false;
@@ -31,5 +40,30 @@ export class UsersComponent implements OnInit {
     else {
 
     }
+  }
+
+  showConfirmDelete(user:User){
+    const confirmDeleteComp=this.componentFactoryRes.resolveComponentFactory(ConfirmDeleteComponent);
+    //confirmDeleteComp.create()
+    const viewContRef= this.container.viewContainer;
+    viewContRef.clear();
+    const compRef=viewContRef.createComponent(confirmDeleteComp);
+
+    //pass the data to dynamic component
+    compRef.instance.usertoDelete=user;
+
+    this.OnConfirmationObs=compRef.instance.OnConfirmation.subscribe(res=>{
+      this.OnConfirmationObs.unsubscribe();
+      viewContRef.clear();
+      if(res)
+      {
+      //delete the user
+      this.userService.users.splice(this.userService.users.indexOf(this.userToDelete),1);
+      }
+    else {
+
+    }
+    });
+
   }
 }
